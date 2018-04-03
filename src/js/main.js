3b820faf6b4074af6f71fc07e0c01832554d4c8e
KevinFuret <kefuret@gmail.com>
2018-04-03 23:27:06 +0200

    :tada: First commit

---
+++ src/js/main.js
@@ -0,0 +1,72 @@
+var sectionActive;
+var sectionInactive;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    sliderSections();
+    changeHeight();
+    changeWidth();
+    console.log('dom loaded');
+});
+
+function getSections() {
+    sectionActive = document.querySelector('.section-active');
+    sectionInactive = document.querySelector('.section-inactive');
+}
+
+function sliderSections() {
+    console.log('sections initialize');
+    
+    getSections();
+
+    sectionActive.removeEventListener("click", changeOrder, false);
+    sectionInactive.addEventListener("click", changeOrder, false);
+}
+
+function changeWidth() {
+    console.log('change width');
+    getSections();
+    
+    sectionInactive.addEventListener("mouseenter", toggleHover);
+    sectionInactive.addEventListener("mouseleave", toggleHover);
+}
+
+function toggleHover(){
+    getSections();
+    sectionInactive.classList.toggle('hover');
+    sectionActive.classList.toggle('hover');
+}
+
+function changeOrder() {
+    console.log("changement section");
+
+    getSections();
+
+    sectionInactive.removeEventListener("mouseenter", toggleHover);
+    sectionInactive.removeEventListener("mouseleave", toggleHover);
+
+    toggleHover();
+
+    var sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.classList.toggle('section-inactive');
+        section.classList.toggle('section-active');
+    });
+
+    sliderSections();
+    changeWidth();
+    getSections();
+}
+
+function changeHeight() {
+    console.log('change height');
+
+    getSections();
+
+    sectionActive.style.height = "100vh";
+
+    var currentHeight = sectionActive.offsetHeight;
+    console.log(currentHeight);
+    sectionInactive.style.height = currentHeight + "px";
+}
+
+// le height du body est le height de la section active
