0eac467df004de10560e386ff9ad2a3cba7607b7
KevinFuret <kefuret@gmail.com>
2018-04-05 20:54:41 +0200

    :bug: Solve a display date problem

--- src/js/main.js
+++ src/js/main.js
@@ -96,8 +96,10 @@ function content() {
                 if (typeof entry.fields.dateEnd != 'undefined') {
                     var dateEndBrut = entry.fields.dateEnd.split('-');
                     var dateEndString = dateEndBrut[1] + '.' + dateEndBrut[0].substr(2);
+                    var dates = dateBeginString + ' - ' + dateEndString;
                 } else {
                     var dateEndString = "";
+                    var dates = dateBeginString;
                 }
 
                 var dates = dateBeginString + ' - ' + dateEndString;
