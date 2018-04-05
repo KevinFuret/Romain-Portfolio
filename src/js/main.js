b1aaa3232adcecc48a7f4c39d746fcd3e98314c7
KevinFuret <kefuret@gmail.com>
2018-04-05 16:28:23 +0200

    :art: Finish Contentful Configuration

--- src/js/main.js
+++ src/js/main.js
@@ -63,14 +63,26 @@ function changeHeight() {
     var currentHeight = sectionActive.offsetHeight;
     sectionInactive.style.height = currentHeight + "px";
 }
+
 function content() {
     var contentful = require('contentful');
+    var showdown = require('showdown');
+    var converter = new showdown.Converter();
 
     var client = contentful.createClient({
         space: 'jszvpzje0cjl',
         accessToken: '7a535b5349a4dd26510674038fd0534ee139d172d34d6eb5fd6bdaf52e44915d'
     });
 
+
+    client.getEntry('2lMA1fjNkQgA04KSSmS6iK')
+        .then(function (entry) {
+
+            // logs the field with ID title
+            var description = converter.makeHtml(entry.fields.description);
+
+            insertText(description , '.section__description', 'section__text')
+        })
 
     client.getEntries({
         'content_type': 'expriences'
@@ -79,23 +91,52 @@ function content() {
             entries.items.forEach(function (entry) {
 
                 var dateBeginBrut = entry.fields.dateBegin.split('-');
-                var dateBeginString = dateBeginBrut[1]+'.'+dateBeginBrut[0].substr(2);
-                console.log(dateBeginString);
+                var dateBeginString = dateBeginBrut[1] + '.' + dateBeginBrut[0].substr(2);
 
-                var dateEndBrut = entry.fields.dateEnd.split('-');
-                var dateEndString = dateEndBrut[1]+'.'+dateEndBrut[0].substr(2);
-                console.log(dateEndString);
+                if (typeof entry.fields.dateEnd != 'undefined') {
+                    var dateEndBrut = entry.fields.dateEnd.split('-');
+                    var dateEndString = dateEndBrut[1] + '.' + dateEndBrut[0].substr(2);
+                } else {
+                    var dateEndString = "";
+                }
 
-                var dates = dateBeginString+' - '+dateEndString;
+                var dates = dateBeginString + ' - ' + dateEndString;
 
-                console.log(JSON.stringify(entry.fields.exprience));
-                insertText('<div class="section__experience"><p class="section__experience-date">'+dates+'</p><p class="section__experience-description">'+entry.fields.exprience+'</p></div>', '.section__experiences');
+                var experience = converter.makeHtml(entry.fields.exprience);
+                insertText(experience, '.section__experiences', 'section__experience', dates);
             })
         })
 }
 
-function insertText(text, place) {
-    document.querySelector(place).innerHTML = text;
-}
+function insertText(text, place, className, date) {
+    date = date || 0;
+    console.log(place);
+    // récupérer le parent
+    var div = document.querySelector(place);
+    // créer une div (sans lui dire ou elle est dans le DOM)
+    var experience = document.createElement('div');
+    // ajouter la classe voulue a l'enfant
+    if (place === '.section__experiences') {
+        experience.classList.add(className);
+        // ajouter le contenu à l'enfant
+        experience.innerHTML = '<p class="section__experience-date">' + date + '</p>' + text;
+    } else if (place === '.section__description'){
+        experience.innerHTML = text;
+        
+    }
 
-// le height du body est le height de la section active
+    // ajouter la div experience dans le DOM, a l'intérieur du parent div
+    div.appendChild(experience);
+    if (place === '.section__experiences') {
+        var descriptions = document.querySelectorAll('.section__experience p:not(:first-child)');
+
+        descriptions.forEach(function (description) {
+            description.classList.add('section__experience-description');
+        });
+    } else {
+        var descriptions = document.querySelectorAll('.section__description > div > *');
+        descriptions.forEach(function (description) {
+            description.classList.add(className);
+        });
+    }
+}
